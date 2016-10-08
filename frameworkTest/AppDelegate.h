2094bcd764d98f55de2a06d654f43226d7210fe0
wangxinjun08231116 <937193295@qq.com>
2016-10-08 10:08:31 +0800

    Initial Commit

---
+++ frameworkTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  frameworkTest
+//
+//  Created by admin on 16/10/8.
+//  Copyright © 2016年 com.WL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ frameworkTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  frameworkTest
+//
+//  Created by admin on 16/10/8.
+//  Copyright © 2016年 com.WL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
